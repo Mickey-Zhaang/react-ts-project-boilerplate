221df51e9ae9e98611b1aa5e065c42fb0be8afe3
Mickey-Zhaang <mickey.zhaang@gmail.com>
2026-01-07 12:23:27 -0500

    Done
    Set Up Basic Project Structure: Minor fixes included
    
    added scrollbar styling
    
    Done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <h1>Application</h1>
     </div>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,1 @@
 // Export all components here
-export { Button } from './Button';
